18ffaf874f3aed20a80461429fba480b7df822f5
1810Ankit <37371679+1810Ankit@users.noreply.github.com>
2018-02-21 11:40:06 +0530

    Did Sorting on input data

--- os.c
+++ os.c
@@ -7,7 +7,21 @@ struct process
 }p[10];
 
 int n;
-
+void sorting()
+{
+struct process temp;
+int i,j;
+for(i=0;i<n-1;i++)
+for(j=i+1;j<n;j++)
+{
+ if(p[i].arrival_time<=p[j].arrival_time)
+ {
+  temp=p[i];
+  p[i]=p[j];
+  p[j]=temp;
+ }
+}
+}
 int main()
 {
  int i,j,time,sum_burst_time=0;
@@ -15,4 +29,16 @@ int main()
  float sum=0,sum1=0;
  printf("Enter no of processes:");
  scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+ p[i].process_number=i;
+ printf("ENTER THE ARRIVAL AND BURST TIME OF THE PROCESS %d\n",(p[i].process_number+1));
+ printf("ENTER THE ARRIVAL TIME: ");
+ scanf("%d",&p[i].arrival_time);
+ printf("ENTER THE BURST TIME: ");
+ scanf("%d",&p[i].burst_time);
+ p[i].flag=0;
+ sum_burst_time+=p[i].burst_time;
+}
+sorting();
 }
