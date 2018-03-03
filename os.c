064d0c73951051e50f534e159272d8f80774792c
1810Ankit <37371679+1810Ankit@users.noreply.github.com>
2018-03-03 11:41:30 +0530

    Processedfor finding biggest priority

--- os.c
+++ os.c
@@ -14,13 +14,43 @@ int i,j;
 for(i=0;i<n-1;i++)
 for(j=i+1;j<n;j++)
 {
- if(p[i].arrival_time<=p[j].arrival_time)
+ if(p[i].arrival_time>p[j].arrival_time)
  {
   temp=p[i];
   p[i]=p[j];
   p[j]=temp;
  }
 }
+}
+void Priority(int a)
+{
+    for(time=p[0].arrival_time;time<sum_burst_time;)
+  {
+
+   float check=-1;
+    int biggest_priority_process;
+  for(i=0;i<n;i++)
+  {
+
+   if(p[i].arrival_time<=time && p[i].flag!=1)
+            {
+              float temp=(p[i].burst_time + (time-p[i].arrival_time))/p[i].burst_time;
+              if(check < temp)
+               {
+                check=temp;
+                biggest_priority_process=i;
+               }
+
+   }
+   }
+
+
+   time+=p[biggest_priority_process].burst_time;
+   p[biggest_priority_process].waiting_time=time-p[biggest_priority_process].arrival_time-p[biggest_priority_process].burst_time;
+   p[biggest_priority_process].turnaround_time=time-p[biggest_priority_process].arrival_time;
+   p[biggest_priority_process].flag=1;
+printf("\nProcess%d\t\t%d\t\t%d\t\t%d\t\t%",(p[loc].process_number+1),p[loc].arrival_time,p[loc].burst_time,p[loc].waiting_time);
+  }
 }
 int main()
 {
@@ -39,6 +69,10 @@ int main()
  scanf("%d",&p[i].burst_time);
  p[i].flag=0;
  sum_burst_time+=p[i].burst_time;
+
 }
 sorting();
+printf("\nName\t\tArrival Time\tBurst Time\tWaiting Time");
+
+printf("\nAverage waiting time:%f\n",sum/n);
 }
